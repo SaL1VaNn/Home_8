5108d3ba15fd5ff0ced704081ef4a45066bdf594
SaL1VaNn <127735395+SaL1VaNn@users.noreply.github.com>
2023-05-03 18:42:39 +0300

    Update sort_program.py

--- sort_program.py
+++ sort_program.py
@@ -14,15 +14,38 @@ music_list = []
 arhives_list = []
 unknown_list = []
 
-
 extention_list = []
 unknown_extenstion = []
 
 
-def sort_progtam(folder):
+def sort_program(folder):
     for item in os.listdir(folder):
         item_path = os.path.join(folder, item)
         if os.path.isfile(item_path):
-            extention = item.split('.')[-1].upper()
-            if extention not in extention_list:
-                extention_list.append(extention)
+            extension = item.split('.')[-1].upper()
+            if extension in IMAGES:
+                images_list.append(item)
+            elif extension in VIDEO_FILE:
+                video_list.append(item)
+            elif extension in DOCUMENT:
+                doc_list.append(item)
+            elif extension in MUSIC:
+                music_list.append(item)
+            elif extension in ARHIVES:
+                arhives_list.append(item)
+            else:
+                unknown_list.append(item)
+        else:
+            sort_program(item_path)
+
+
+folder_path = "C:\\Users\\Admin\\Desktop"
+sort_program(folder_path)
+
+
+print("Images:", images_list)
+print("Videos:", video_list)
+print("Documents:", doc_list)
+print("Music:", music_list)
+print("Archives:", arhives_list)
+print("Unknown files:", unknown_list)
