e54c84c08fb4f04129ec402c18445053637a1aa8
SaL1VaNn <127735395+SaL1VaNn@users.noreply.github.com>
2023-05-24 20:17:25 +0300

    Переробив код

--- birthday.py
+++ birthday.py
@@ -1,9 +1,17 @@
 from datetime import datetime, timedelta
 
+users = [
+    {"name": "John", "birthday": datetime(1990, 5, 12)},
+    {"name": "Alice", "birthday": datetime(1985, 8, 20)},
+    {"name": "David", "birthday": datetime(1992, 4, 5)},
+    {"name": "Sarah", "birthday": datetime(1998, 10, 3)},
+    {"name": "Michael", "birthday": datetime(1995, 7, 15)},
+]
+
 
 def get_birthdays_per_week(users):
-    today = datetime().today().date()
-    start_week = today - timedelta(days=today().weekday())
+    today = datetime.today().date()
+    start_week = today - timedelta(days=today.weekday())
     end_week = start_week + timedelta(days=6)
 
     weekdays = [
@@ -23,9 +31,23 @@ def get_birthdays_per_week(users):
 
         user_birth = []
         for user in users:
-            birthdays = user["birthdays"].date()
-            if birthdays >= current_day and birthdays <= end_week:
+            birthday = user["birthday"].date()
+            if birthday >= current_day and birthday <= end_week:
                 user_birth.append(user["name"])
 
-            if user_birth:
-                print(f"{weekdays[i]}: {', '.join(user_birth)}")
+        if user_birth:
+            print(f"{weekdays[i]}: {', '.join(user_birth)}")
+
+
+get_birthdays_per_week(users)
+
+
+# users = [
+#     {"name": "John", "birthday": datetime(1990, 5, 12)},
+#     {"name": "Alice", "birthday": datetime(1985, 8, 20)},
+#     {"name": "David", "birthday": datetime(1992, 4, 5)},
+#     {"name": "Sarah", "birthday": datetime(1998, 10, 3)},
+#     {"name": "Michael", "birthday": datetime(1995, 7, 15)},
+# ]
+
+# get_birthdays_per_week(users)
