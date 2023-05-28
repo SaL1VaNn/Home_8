87a4684d330a5ca6d2493845fa62a6506b66d2fd
SaL1VaNn <127735395+SaL1VaNn@users.noreply.github.com>
2023-05-28 12:44:20 +0300

    новлений код

--- birthday.py
+++ birthday.py
@@ -1,53 +1,37 @@
 from datetime import datetime, timedelta
 
+
+def get_birthdays_per_week(users):
+    current_date = datetime.now().date()
+    start_of_week = current_date - timedelta(days=current_date.weekday())
+
+    birthdays_per_week = {}
+
+    for user in users:
+        name = user["name"]
+        birthday = user["birthday"].date()
+
+        birthday_weekday = start_of_week + timedelta(days=birthday.weekday())
+
+        if birthday_weekday < start_of_week + timedelta(days=7):
+            if birthday_weekday not in birthdays_per_week:
+                birthdays_per_week[birthday_weekday] = [name]
+            else:
+                birthdays_per_week[birthday_weekday].append(name)
+
+    for day, names in birthdays_per_week.items():
+        weekday = day.strftime("%A")
+        names_str = ", ".join(names)
+        print(f"{weekday}: {names_str}")
+
+
 users = [
-    {"name": "John", "birthday": datetime(1990, 5, 12)},
-    {"name": "Alice", "birthday": datetime(1985, 8, 20)},
-    {"name": "David", "birthday": datetime(1992, 4, 5)},
-    {"name": "Sarah", "birthday": datetime(1998, 10, 3)},
-    {"name": "Michael", "birthday": datetime(1995, 7, 15)},
+    {"name": "John", "birthday": datetime(1990, 5, 24)},
+    {"name": "Alice", "birthday": datetime(1985, 5, 26)},
+    {"name": "David", "birthday": datetime(1992, 5, 28)},
+    {"name": "Sarah", "birthday": datetime(1998, 5, 29)},
+    {"name": "Michael", "birthday": datetime(1995, 5, 30)},
 ]
 
 
-def get_birthdays_per_week(users):
-    today = datetime.today().date()
-    start_week = today - timedelta(days=today.weekday())
-    end_week = start_week + timedelta(days=6)
-
-    weekdays = [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday",
-    ]
-
-    for i in range(7):
-        current_day = start_week + timedelta(days=i)
-        if current_day > end_week:
-            break
-
-        user_birth = []
-        for user in users:
-            birthday = user["birthday"].date()
-            if birthday >= current_day and birthday <= end_week:
-                user_birth.append(user["name"])
-
-        if user_birth:
-            print(f"{weekdays[i]}: {', '.join(user_birth)}")
-
-
 get_birthdays_per_week(users)
-
-
-# users = [
-#     {"name": "John", "birthday": datetime(1990, 5, 12)},
-#     {"name": "Alice", "birthday": datetime(1985, 8, 20)},
-#     {"name": "David", "birthday": datetime(1992, 4, 5)},
-#     {"name": "Sarah", "birthday": datetime(1998, 10, 3)},
-#     {"name": "Michael", "birthday": datetime(1995, 7, 15)},
-# ]
-
-# get_birthdays_per_week(users)
